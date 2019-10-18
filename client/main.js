723a505eb1bfe0045097c3de804340236914e16f
Anderson Day <dayander@msu.edu>
2019-10-18 14:50:23 -0400

    prod ready

--- client/main.js
+++ client/main.js
@@ -16,6 +16,14 @@ const initialState = typeof window !== "undefined" && window && window.INITIAL_S
 
 
 export default function App(){
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+
+
     return(
         <Context.Provider value={{...initialState, ...ContextState()}}>
             <Router history={history}>
